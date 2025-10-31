855c114aaf97b82e373ce19720de49eb2d07415b
Batuhan Kav <batuhankav@gmail.com>
2025-10-31 16:37:26 +0100

    Extend the experiment class

--- src/DatabankLib/experiment.py
+++ src/DatabankLib/experiment.py
@@ -4,6 +4,7 @@
 :description: Module for handling experimental data entries in the databank.
 """
 
+import json
 import os
 from abc import ABC, abstractmethod
 from collections.abc import MutableSet
@@ -121,15 +122,18 @@ class Experiment(ABC):
 
     _exp_id: str
     _metadata: dict | None = None
+    _data: dict | None = None
 
-    def __init__(self, exp_id: str):
+    def __init__(self, exp_id: str, path: str):
         """
         Initializes the Experiment object.
 
         :param exp_id: The unique identifier for the experiment, which also
                        corresponds to its directory name.
+        :param path: The absolute path to the experiment's directory.
         """
         self._exp_id = exp_id
+        self._path = path
         self._populate_meta_data()
 
     def _get_path(self) -> str:
@@ -138,7 +142,7 @@ class Experiment(ABC):
 
         :return: The path to the experiment's data folder.
         """
-        return os.path.join(NMLDB_EXP_PATH, self._exp_id)
+        return self._path
 
     def _populate_meta_data(self) -> None:
         """
@@ -173,6 +177,20 @@ class Experiment(ABC):
         """
         return self._exp_id
 
+    @property
+    @abstractmethod
+    def data(self) -> dict:
+        """
+        Provides access to the experiment's data.
+        """
+
+    @property
+    @abstractmethod
+    def schema(self) -> str:
+        """
+        Path to the JSON schema for validation.
+        """
+
     def __getitem__(self, key: str):
         """
         Allows dictionary-style access to the metadata.
@@ -187,3 +205,114 @@ class Experiment(ABC):
 
     def __hash__(self):
         return hash(self.id.upper())
+
+    def get_lipids(self, molecules: set) -> list[str]:
+        """
+        Get lipids from the experiment's metadata.
+
+        :param molecules: A set of molecule names to filter against.
+        :return: A list of lipid names.
+        """
+        return [k for k in self.metadata.get("MOLAR_FRACTIONS", {}) if k in molecules]
+
+    def get_ions(self, ions: list) -> list[str]:
+        """
+        Get ions from the experiment's metadata.
+
+        :param ions: A list of ion names to check for.
+        :return: A list of ion names present in the experiment.
+        """
+        exp_ions: list[str] = []
+        for key in ions:
+            try:
+                if self.metadata.get("ION_CONCENTRATIONS", {}).get(key, 0) != 0:
+                    exp_ions.append(key)
+            except KeyError:
+                continue
+            try:
+                if key in self.metadata.get("COUNTER_IONS", []):
+                    exp_ions.append(key)
+            except (TypeError, KeyError):
+                continue
+        return exp_ions
+
+
+class OPExperiment(Experiment):
+    """
+    Represents an order parameter experiment.
+    """
+
+    _schema_path = "path/to/op_schema.json"  # Placeholder
+
+    @property
+    def data(self) -> dict:
+        if self._data is None:
+            self._data = {}
+            for fname in os.listdir(self._get_path()):
+                if fname.endswith("_Order_Parameters.json"):
+                    molecule_name = fname.replace("_Order_Parameters.json", "")
+                    fpath = os.path.join(self._get_path(), fname)
+                    with open(fpath) as json_file:
+                        self._data[molecule_name] = json.load(json_file)
+        return self._data
+
+    @property
+    def schema(self) -> str:
+        return self._schema_path
+
+
+class FFExperiment(Experiment):
+    """
+    Represents a form factor experiment.
+    """
+
+    _schema_path = "path/to/ff_schema.json"  # Placeholder
+
+    @property
+    def data(self) -> dict:
+        if self._data is None:
+            self._data = {}
+            for fname in os.listdir(self._get_path()):
+                if fname.endswith("_FormFactor.json"):
+                    fpath = os.path.join(self._get_path(), fname)
+                    with open(fpath) as json_file:
+                        self._data = json.load(json_file)
+                    break  # Assuming one form factor file per experiment
+        return self._data
+
+    @property
+    def schema(self) -> str:
+        return self._schema_path
+
+
+class ExperimentCollection(Collection):
+    """
+    A collection of experiments.
+    """
+
+    def _test_item_type(self, item: Any) -> bool:
+        return isinstance(item, Experiment)
+
+    def _create_item(self, name: str) -> Experiment:
+        raise NotImplementedError("Cannot create an experiment by name. Use load_from_data.")
+
+    @staticmethod
+    def load_from_data() -> "ExperimentCollection":
+        """
+        Loads experiment data from the designated directory and returns a set of experiments.
+        """
+        collection = ExperimentCollection()
+        for exp_type, exp_cls in [("OrderParameters", OPExperiment), ("FormFactors", FFExperiment)]:
+            path = os.path.join(NMLDB_EXP_PATH, exp_type)
+            if not os.path.isdir(path):
+                continue
+            for subdir, _, files in os.walk(path):
+                if "README.yaml" in files:
+                    exp_id = os.path.basename(subdir)
+                    try:
+                        exp = exp_cls(exp_id, subdir)
+                        collection.add(exp)
+                    except FileNotFoundError:
+                        # Log this error?
+                        pass
+        return collection
